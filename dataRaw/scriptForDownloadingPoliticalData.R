810726f7bc4591e31e6db363efae3e41d01e45bb
Adrien Ratsimbaharison <ratsimb@gmail.com>
2022-05-06 03:07:31 -0400

    5/6/2022: commit on downloading and saving Freedom House data.

---
+++ dataRaw/scriptForDownloadingPoliticalData.R
@@ -0,0 +1,34 @@
+# Script for downloading political data from democracyData
+
+library(democracyData)
+library(tidyverse)
+
+# Downloading Freedoom House data on political rights (pr) and civil liberty (cl)
+
+fh <- download_fh(verbose = FALSE)
+fh
+names(fh)
+
+freedomHouseData <- select(fh, country = fh_country, date = year, politicalRights = pr, civilLiberties = cl, fhCombinedScore = fh_total, fhCombinedScoreReversed = fh_total_reversed,status) %>%
+  filter(date > 1995)
+
+head(freedomHouseData)
+View(freedomHouseData)
+
+# saving the freedom house data in the directory
+freedomHouseDataRaw <- write_csv(freedomHouseData, "dataRaw/freedomHouseDataRaw.csv")
+
+
+# Downloading polity5 data on institutionalized democracy, institutionalized 
+
+polity5Data <- download_polity_annual(verbose = FALSE)
+polity5Data
+names(polity5Data)
+
+freedomHouseData <- select(polity5Data, country = fh_country, date = cyear, ) %>%
+  filter(date > 1995)
+
+head(polity5Data)
+View(polity5Data)
+
+polity5DataRaw <- write_csv(polity5Data, "dataRaw/polity5DataRaw.csv")
